f7230eaed19ef883fb0ddd4c03029296f0d9485a
Wira <zmuhamad064@gmail.com>
2025-06-15 11:47:54 +0700

    update

--- resources/views/tulisan.blade.php
+++ resources/views/tulisan.blade.php
@@ -31,7 +31,7 @@
                                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wides">Edit
                                         </button>
                                         <button type="submit"
-                                            class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wides">Delete
+                                            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wides">Delete
                                         </button>
                                     @endif
                                 </td>
